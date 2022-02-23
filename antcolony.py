40a7e840d1f98a9d61d9f23c0244fab34bc25ebd
Yuichi Tokumoto <yuichi@Yuichis-MacBook-Air.local>
2022-02-23 10:20:59 +0100

    Changed file path in function run

--- antcolony.py
+++ antcolony.py
@@ -10,9 +10,9 @@ def make(simd="avx2", Olevel="O3"):
 
 
 def run(n1, n2, n3, num_thread, iteration, b1, b2, b3):
-    filename = os.listdir("~/Documents/iso3dfd-st7/bin/")
+    filename = os.listdir("./iso3dfd-st7/bin/")
     p = subprocess.Popen([
-        f"~/Documents/iso3dfd-st7/bin/{filename}",
+        f"./iso3dfd-st7/bin/{filename}",
         str(n1),
         str(n2),
         str(n3),
