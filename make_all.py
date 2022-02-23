2bed1003976f4ecf1113090442012830aa2b184e
Yuichi Tokumoto <yuichi@Yuichis-MacBook-Air.local>
2022-02-23 16:35:22 +0100

    Changed make.config and added make_all.py - Bug fixes

--- make_all.py
+++ make_all.py
@@ -7,11 +7,6 @@ comb_build = list(itertools.product(["avx", "avx2", 'avx512', 'sse'],
 
 def make(simd="avx2", Olevel="-O3"):
     os.chdir("./iso3dfd-st7/")
-    try:
-        os.system(f"make clean")
-    except Exception as e:
-        print(e)
-        pass
     os.system(f"make build simd={simd} Olevel={Olevel}")
     os.chdir("..")
 
