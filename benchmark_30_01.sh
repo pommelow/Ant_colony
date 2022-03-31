f0142bd9c83150329abab0f5476939593a3a03eb
yu9800 <yuichitokumoto@usp.br>
2022-03-31 08:52:20 +0200

    more results

--- benchmark_30_01.sh
+++ benchmark_30_01.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
-#SBATCH --nodes=4
+#SBATCH --nodes=8
 #SBATCH --job-name=test_mpi2
 #SBATCH --partition=cpu_prod
 #SBATCH --time=02:00:00
 #SBATCH --qos=16nodespu
 #SBATCH --exclusive
-#SBATCH --ntasks 128
+#SBATCH --ntasks 256
 
-mpirun -np 4 -ppn 1 --bind-to socket python main.py --distribution exchange_all
+mpirun -np 8 -ppn 1 --bind-to socket python main.py --distribution exchange_all --local-search simulated_annealing --nb_ant 15 --tau_min 0.5 --tau_max 10 --nb-to-update 15
 
 
--- main.py
+++ main.py
@@ -11,30 +11,47 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--alpha", type=float, default=1, help="Control the pheromons")
+parser.add_argument("--alpha", type=float, default=1,
+                    help="Control the pheromons")
 parser.add_argument("--beta", type=float, default=1, help="Not used")
-parser.add_argument("--rho", type=float, default=0.1, help="Control the pheromons evaporation")
-parser.add_argument("--Q", type=float, default=1, help="Control the reward of the best ants")
-parser.add_argument("--nb_ant", type=int, default=25, help="Number of ants in each generation")
-parser.add_argument("--method", type=str, default="mmas", choices=["basic", "asrank", "elitist", "mmas"], help="")
-parser.add_argument("--local-search", type=str, default="identity", choices=["identidy", "greedy", "simulated_annealing", "tabu"], help="identity means no local search")
-parser.add_argument("--max_time", type=float, default=1800, help="maximum execution time of the ant colony")
+parser.add_argument("--rho", type=float, default=0.1,
+                    help="Control the pheromons evaporation")
+parser.add_argument("--Q", type=float, default=1,
+                    help="Control the reward of the best ants")
+parser.add_argument("--nb_ant", type=int, default=25,
+                    help="Number of ants in each generation")
+parser.add_argument("--method", type=str, default="mmas",
+                    choices=["basic", "asrank", "elitist", "mmas"], help="")
+parser.add_argument("--local-search", type=str, default="identity", choices=[
+                    "identidy", "greedy", "simulated_annealing", "tabu"], help="identity means no local search")
+parser.add_argument("--max_time", type=float, default=1800,
+                    help="maximum execution time of the ant colony")
 
 # method parameters
-parser.add_argument("--nb-to-update", type=int, default=12, help="nb of ants to update in asrank ans mmas methods")
-parser.add_argument("--tau_min", type=float, default=0.5, help="tau min in mmas")
-parser.add_argument("--tau_max", type=float, default=10, help="tau max in mmas")
+parser.add_argument("--nb-to-update", type=int, default=12,
+                    help="nb of ants to update in asrank ans mmas methods")
+parser.add_argument("--tau_min", type=float,
+                    default=0.5, help="tau min in mmas")
+parser.add_argument("--tau_max", type=float,
+                    default=10, help="tau max in mmas")
 
 # local search parameters
-parser.add_argument("--time_local", type=float, default=600, help="time before local search")
-parser.add_argument("--kmax", type=float, default=3, help="max iteration for each local search method")
-parser.add_argument("--t0", type=float, default=50, help="initial temperature in simulated annealing")
-parser.add_argument("--t-decay", type=float, default=0.98, help="temperature decay in simulated annealing")
-parser.add_argument("--t_min", type=float, default=0.01, help="minimum temperature in simulated annealing")
-parser.add_argument("--tabu-size", type=int, default=5, help="size of memory in tabu search")
+parser.add_argument("--time_local", type=float, default=600,
+                    help="time before local search")
+parser.add_argument("--kmax", type=float, default=3,
+                    help="max iteration for each local search method")
+parser.add_argument("--t0", type=float, default=50,
+                    help="initial temperature in simulated annealing")
+parser.add_argument("--t-decay", type=float, default=0.98,
+                    help="temperature decay in simulated annealing")
+parser.add_argument("--t_min", type=float, default=0.01,
+                    help="minimum temperature in simulated annealing")
+parser.add_argument("--tabu-size", type=int, default=5,
+                    help="size of memory in tabu search")
 
 # distribution parameters
-parser.add_argument("--distribution", type=str, default="independent", choices=["independent", "exchange_all"], help="how process communicate with each other")
+parser.add_argument("--distribution", type=str, default="independent", choices=[
+                    "independent", "exchange_all"], help="how process communicate with each other")
 
 
 args = parser.parse_args()
@@ -43,18 +60,30 @@ print(args)
 block_min = 16
 block_max = 256
 
-#Graph of the the Ant colony
+# Graph of the the Ant colony
 levels = [("init", ["init"]),
-            ("n1", list(range(256, 1024+1, 32))),
-            ("n2", list(range(256, 1024+1, 32))),
-            ("n3", list(range(256, 1024+1, 32))),
-            ("simd", ["avx", "avx2", "avx512"]),
-            ("Olevel", ["-O2", "-O3", "-Ofast"]),
-            ("num_thread", [16]),
-            ("b1", list(range(128, 512+1, 16))),
-            ("b2", list(range(8, 64+1, 1))),
-            ("b3", list(range(128, 256+1, 1)))
-            ]
+          ("n1", list(range(256, 1024+1, 32))),
+          ("n2", list(range(256, 1024+1, 32))),
+          ("n3", list(range(256, 1024+1, 32))),
+          ("simd", ["avx", "avx2", "avx512"]),
+          ("Olevel", ["-O2", "-O3", "-Ofast"]),
+          ("num_thread", [16]),
+          ("b1", list(range(128, 512+1, 16))),
+          ("b2", list(range(8, 64+1, 1))),
+          ("b3", list(range(128, 256+1, 1)))
+          ]
+
+# levels = [("init", ["init"]),
+#           ("n1", [512]),
+#           ("n2", [512]),
+#           ("n3", [1024]),
+#           ("simd", ["avx", "avx2", "avx512"]),
+#           ("Olevel", ["-O2", "-O3", "-Ofast"]),
+#           ("num_thread", [16]),
+#           ("b1", list(range(128, 512+1, 16))),
+#           ("b2", list(range(8, 64+1, 1))),
+#           ("b3", list(range(128, 256+1, 1)))
+#           ]
 
 # choose the local search method
 if args.local_search == "identity":
@@ -62,9 +91,11 @@ if args.local_search == "identity":
 elif args.local_search == "greedy":
     local_search_method = GreedySearch(args.kmax)
 elif args.local_search == "simulated_annealing":
-    local_search_method = SimulatedAnnealing(args.kmax, args.t0, lambda t: max(args.t_min, args.t_decay * t))
+    local_search_method = SimulatedAnnealing(
+        args.kmax, args.t0, lambda t: max(args.t_min, args.t_decay * t))
 elif args.local_search == "tabu":
-    local_search_method = RandomizedTabu(args.tabu_size, args.k_max, args.k_max, args.t0, lambda t: max(args.t_min, args.t_decay * t))
+    local_search_method = RandomizedTabu(
+        args.tabu_size, args.k_max, args.k_max, args.t0, lambda t: max(args.t_min, args.t_decay * t))
 
 if args.distribution == "independent":
     communication = IndependentColonies()
@@ -72,14 +103,16 @@ if args.distribution == "exchange_all":
     communication = ExchangeAll()
 
 
-method_args = {"nb_to_update": args.nb_to_update, "tau_min": args.tau_min, "tau_max": args.tau_max}
+method_args = {"nb_to_update": args.nb_to_update,
+               "tau_min": args.tau_min, "tau_max": args.tau_max}
 
-#Launch _ colony x times to reduce variance in results
+# Launch _ colony x times to reduce variance in results
 
 for _ in range(1):
 
-    #creation of the colony
-    ant_colony = AntColony(args.alpha, args.beta, args.rho, args.Q, args.nb_ant, levels, args.method, Identity(), **method_args)
+    # creation of the colony
+    ant_colony = AntColony(args.alpha, args.beta, args.rho, args.Q,
+                           args.nb_ant, levels, args.method, Identity(), **method_args)
 
     best_cost = np.inf
     best_path = []
@@ -108,22 +141,23 @@ for _ in range(1):
                     desc="Epoch Me: "+str(communication.Me))
 
     to_save = []
-    #run the colony until max time
+    # run the colony until max time
     while time()-top < args.max_time:
         top1 = time()
-        #add local method after "time local"
+        # add local method after "time local"
         if time()-top > args.time_local:
             ant_colony.local_search_method = local_search_method
 
         communication.on_epoch_begin()
 
-        #do 1 epoch
+        # do 1 epoch
         pathes, performances = ant_colony.epoch()
-        #share performances between different nodes
+        # share performances between different nodes
         communication.comm.Barrier()
-        pathes, performances = communication.on_epoch_end(ant_colony, pathes, performances)
+        pathes, performances = communication.on_epoch_end(
+            ant_colony, pathes, performances)
 
-        #get the performance of the colony over time
+        # get the performance of the colony over time
         if performances[0] < best_cost:
             best_path = pathes[0]
             best_cost = performances[0]
@@ -135,7 +169,7 @@ for _ in range(1):
             save_results(zip(pathes, performances), path_dir)
             to_save.append(zip(pathes, performances))
             pbar.update(int(time()-top1))
-        
+
         if best_path == pathes[0]:
             same_solution_counter += 1
             if same_solution_counter >= 5:
@@ -143,7 +177,8 @@ for _ in range(1):
         else:
             same_solution_counter = 0
 
-    best_path, best_cost = communication.last_communication(best_path, best_cost)
+    best_path, best_cost = communication.last_communication(
+        best_path, best_cost)
 
     if communication.Me == 0:
         dict_ants = {}
